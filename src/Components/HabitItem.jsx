62a55b182d36c5e0a5e921702e91cbc595691126
Bhuvaneshwaran <bhuvanesh.s227@gmail.com>
2025-06-04 00:13:45 +0530

    Updated

--- src/Components/HabitItem.jsx
+++ src/Components/HabitItem.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { removeHabit, toggleDay } from '../Store/habitSlice';
 import Lottie from 'lottie-react';
@@ -6,6 +6,7 @@ import sparkleAnimation from '../assets/Animation - 1748960740817.json'
 import deleteIcon from '../assets/icons8-delete-30.png';
 
 const HabitItem = (props) => {
+  const [showSparke, setShowSparkle] = useState(false);
   const dispatch = useDispatch();
   const handleToggle = (day) => {
     dispatch(toggleDay({ habitId: props.id, day }))
@@ -21,11 +22,18 @@ const HabitItem = (props) => {
 
   const percentage = Math.round((count / 7) * 100);
 
+  useEffect(() => {
+    if (percentage === 100) {
+      setShowSparkle(true);
+      const timeout = setTimeout(() => setShowSparkle(false), 1000);
+      return () => clearTimeout(timeout);
+    }
+  }, [percentage])
   return (
     <div className="bg-white p-4 rounded-md shadow-md space-y-4">
       {
-        percentage === 100 && (
-          <div className="absolute top-0 left-1/2 transform -translate-x-1/2 w-full h-54 z-10">
+        showSparke && (
+          <div className="absolute top-[-100px] left-1/2 transform -translate-x-1/2 w-full h-24 z-10">
             <Lottie animationData={sparkleAnimation} loop={false} />
           </div>
         )
@@ -33,18 +41,18 @@ const HabitItem = (props) => {
       <div className="flex justify-between items-center">
         <div>
           <h2 className="text-xl font-semibold">{props.title}</h2>
-          <p className="text-[12px] text-white rounded-2xl p-1 bg-gray-400 text-center">{props.category}</p>
+          <p className="text-[12px] text-white rounded-2xl p-1 bg-gray-400 text-center mt-2">{props.category}</p>
         </div>
         <button
           onClick={handleRemove}
           className="bg-red-500 text-white px-3 py-1 flex justify-between  items-center rounded hover:bg-red-600 cursor-pointer"
         >
           <img src={deleteIcon} alt="" />
-          <span className='hidden sm:inline'>Delete</span>
+          {/* <span className='hidden sm:inline'>Delete</span> */}
         </button>
       </div>
 
-      <div className="flex flex-wrap gap-8 justify-between">
+      <div className="flex flex-wrap gap-8 bg-blue-950 p-4 rounded-2xl">
         {Object.entries(props.week).map(([day, checked]) => (
           <>
             <label key={day} className="flex flex-wrap items-center gap-1">
@@ -52,15 +60,14 @@ const HabitItem = (props) => {
                 type="checkbox"
                 checked={!!checked}
                 onChange={() => handleToggle(day)}
-                className="w-4 h-4"
+                className="w-10 h-10 bg-white cursor-pointer appearance-none checked:bg-blue-500 rounded-2xl border-2 border-gray-400"
               />
-              <span>{day}</span>
-            </label>
 
+            </label>
           </>
 
         ))}
-        <div className="flex flex-col items-center mt-3">
+        <div className="flex flex-col items-center ">
           <div className="relative w-15 h-15 rounded-full transition-all" style={{ background: `conic-gradient(#3b82f6 0% ${percentage}%, #d1d5db ${percentage}% 100%) `, transition: "all" }}>
             {/* Inner circle to create border effect */}
             <div className="absolute top-2 left-2 w-11 h-11 bg-white rounded-full flex items-center justify-center">
